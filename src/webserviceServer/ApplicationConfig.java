51ecb24d82b7c85a0af8f6a3bf509e03b354f283
kreshan <kreshan@krshan.epic.lk>
2016-08-11 12:21:40 +0530

    ss

--- src/webserviceServer/ApplicationConfig.java
+++ src/webserviceServer/ApplicationConfig.java
@@ -30,7 +30,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(ssss.GenericResource.class);
+        resources.add(webserviceServer.GenericResource.class);
     }
     
 }
